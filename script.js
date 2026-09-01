3539bc5e8b662aa7ce782b0999ea3c482a396e2b
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-09-01 06:36:10 +0000

    Set width and height to container and remove min-width and min-height from grid

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function addChild(k) {
     for(let i=0; i<k; i++){
         let subDiv = document.createElement("div");
         subDiv.classList.add("grid");
-        let divSize = subdiv.width;
-        console.log(divSize);
         for(let j = 0; j < k ; j++) {
             let superSubDiv = document.createElement("div");
             superSubDiv.classList.add("meow");
@@ -17,3 +15,6 @@ function addChild(k) {
 }
 
 addChild(16);
+
+let divSize = subDiv.width;
+    console.log(divSize);
