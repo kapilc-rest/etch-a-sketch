5376c0344221a89f34869095bb4b8174374c3883
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-09-01 06:12:09 +0000

    Remove addMoreChild() and adjoined code to addChild instead

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let container = document.querySelector("#container");
 
-function addChild(j) {
-    for(let i=0; i<j; i++){
+function addChild(k) {
+    for(let i=0; i<k; i++){
         let subDiv = document.createElement("div");
         subDiv.classList.add("grid");
+        for(let j = 0; j < k ; j++) {
+            let superSubDiv = document.createElement("div");
+            superSubDiv.classList.add("meow");
+            subDiv.appendChild(superSubDiv);
+        }
         container.appendChild(subDiv);
         console.log(i);
     }
@@ -12,9 +17,6 @@ function addChild(j) {
 function addMoreChild() {
     addChild(16);
     for(let i = 0; i < 16; i++){
-        let superSubDiv = document.createElement("div");
-        superSubDiv.classList.add("meow");
-        subDiv.appendChild(superSubDiv);
         console.log(i);
     }
 }
